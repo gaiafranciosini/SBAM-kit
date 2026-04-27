48656c941b12d9f983b629fc3910f1d47f765755
Gaia Franciosini <gfrancio@lnf.infn.it>
2026-04-27 17:27:51 +0200

    errore

--- Scripts/PHOTONS_FLASH/ComputeDRVH/ComputeDRVH.cpp
+++ Scripts/PHOTONS_FLASH/ComputeDRVH/ComputeDRVH.cpp
@@ -14,19 +14,19 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    std::vector<std::string> filename;
+    vector<string> filename;
     string dosefile = "";
     string roitype = "";
     string fileLabel = "";
     string dir = ".";
-    float Dgoal = 0.0f;
     int npulse = 1;
-    float prf = 1.0f; // Default a 1 per evitare divisioni per zero
+    float prf = 1.0f; 
     
     int nn[3];
     float x0[3], L[3], hs[3];
     float left[3], up[3], front[3];
-    int binningDVH = 200;
+    int binningDVH = 400; 
+    float maxRangeX = 200.0f; 
     bool plot = false;
     string allROINames = "";
 
@@ -35,7 +35,7 @@ int main(int argc, char *argv[]) {
         if (arg == "-plot") {
             plot = true;
         } else if (arg == "-roi") {
-            while (i + 1 < argc && argv[i + 1][0] != '-') {
+            while (i + 1 < argc && argv[i+1][0] != '-') {
                 filename.push_back(argv[++i]);
             }
         } else if (arg == "-dose" && i + 1 < argc) {
@@ -47,32 +47,39 @@ int main(int argc, char *argv[]) {
         } else if (arg == "-fileLabel" && i + 1 < argc) {
             fileLabel = argv[++i];
         } else if (arg == "-npulse" && i + 1 < argc) {
-            npulse = std::stoi(argv[++i]);
+            npulse = stoi(argv[++i]);
         } else if (arg == "-prf" && i + 1 < argc) {
-            prf = std::stof(argv[++i]);
+            prf = stof(argv[++i]);
         }
     }
 
     if (filename.empty() || dosefile.empty() || roitype.empty() || fileLabel.empty()) {
-        std::cerr << "Error: Missing required arguments." << std::endl;
+        cerr << "Error: Missing arguments." << endl;
         return 1;
     }
 
-    // Calcolo del tempo totale T = npulse / prf
     float T = (prf > 0) ? (static_cast<float>(npulse) / prf) : 1.0f;
-    cout << "Calculated T (npulse/prf): " << T << " s" << endl;
-
+    
     vector<float32> dose;
     read_mhd(dosefile, dose, nn, x0, L, left, up, front);
 
-    // Normalizzazione della dose: Dose = Dose / T
     for (size_t i = 0; i < dose.size(); ++i) {
         dose[i] /= T;
     }
 
-    string doseRateOut = dir + "/DoseRate" + fileLabel + ".mhd";
-    cout << "Saving DoseRate field to: " << doseRateOut << endl;
+    // Costruzione nome file: nome_originale + _ + fileLabel
+    size_t lastDotDose = dosefile.find_last_of(".");
+    string baseName = (lastDotDose == string::npos) ? dosefile : dosefile.substr(0, lastDotDose);
+    size_t lastSlashDose = baseName.find_last_of("/");
+    string simpleBaseName = (lastSlashDose == string::npos) ? baseName : baseName.substr(lastSlashDose + 1);
+
+    string doseRateOutFile = dir + "/" + simpleBaseName + "_" + fileLabel;
     
+    // Configurazione parametri write_mhd come richiesto
+    string dtype = "MET_FLOAT"; 
+    write_mhd(doseRateOutFile, dtype, (char*)dose.data(), nn, x0, L, left, up, front, true);
+    
+    // Passaggio a cm per calcolo DVH
     for (int i = 0; i < 3; i++) {
         L[i] /= 10.0;
         x0[i] /= 10.0;
@@ -81,14 +88,11 @@ int main(int argc, char *argv[]) {
     for (size_t i = 0; i < filename.size(); i++) {
         vector<int16> roi_int;
         vector<float32> roi_float;
-        int nVoxelROI = 0;
         float voxelVolume = 0.0f;
         float ROIVolume = 0.0f;
 
-        size_t lastSlash = filename[i].find_last_of("/");
-        size_t lastDot = filename[i].find_last_of(".");
-        size_t start = (lastSlash == string::npos) ? 0 : lastSlash + 1;
-        size_t end = (lastDot == string::npos) ? filename[i].size() : lastDot;
+        size_t start = filename[i].find_last_of("/") + 1;
+        size_t end = filename[i].find_last_of(".");
         string ROI_name = filename[i].substr(start, end - start);
         
         allROINames += ROI_name + (i == filename.size() - 1 ? "" : " ");
@@ -101,7 +105,6 @@ int main(int argc, char *argv[]) {
             for (size_t n = 0; n < dose.size(); n++) {
                 if (roi_int[n] > 0) {
                     ROIVolume += roi_int[n] * voxelVolume;
-                    nVoxelROI++;
                     D_masked[n] = dose[n];
                 }
             }
@@ -112,29 +115,34 @@ int main(int argc, char *argv[]) {
             for (size_t n = 0; n < dose.size(); n++) {
                 if (roi_float[n] > 0) {
                     ROIVolume += roi_float[n] * voxelVolume;
-                    nVoxelROI++;
                     D_masked[n] = dose[n];
                 }
             }
         }
 
-        float maxDose = 0;
-        for(double d : D_masked) if(d > maxDose) maxDose = d;
-        
-        float binSize = binningDVH;
-        int nbin = static_cast<int>(maxDose / binSize) + 2;
-        if(nbin < binningDVH) nbin = binningDVH;
+        int nbin = binningDVH; 
+        float binSize = maxRangeX / (float)nbin; 
 
         vector<float> bins(nbin, 0.0);
         vector<float> hist(nbin, 0.0);
 
-        for (int b = 0; b < nbin; b++) bins[b] = (b + 0.5) * binSize;
+        for (int b = 0; b < nbin; b++) {
+            bins[b] = (b + 0.5) * binSize;
+        }
 
         for (size_t n = 0; n < D_masked.size(); n++) {
             if (D_masked[n] > 0) {
-                int imax = static_cast<int>(D_masked[n] / binSize);
+                float currentDose = static_cast<float>(D_masked[n]);
                 float weight = (roitype == "int") ? static_cast<float>(roi_int[n]) : roi_float[n];
-                for (int j = 0; j <= imax && j < nbin; j++) {
+                
+                int b_idx;
+                if (currentDose >= maxRangeX) {
+                    b_idx = nbin - 1;
+                } else {
+                    b_idx = static_cast<int>(currentDose / binSize);
+                }
+
+                for (int j = 0; j <= b_idx && j < nbin; j++) {
                     hist[j] += weight * voxelVolume;
                 }
             }
@@ -143,14 +151,15 @@ int main(int argc, char *argv[]) {
         string outPath = dir + "/" + ROI_name + fileLabel + ".txt";
         ofstream fout(outPath);
         for (int b = 0; b < nbin; b++) {
-            fout << bins[b] << " " << (hist[b] / ROIVolume) * 100.0 << endl;
+            if (ROIVolume > 0) {
+                fout << bins[b] << " " << (hist[b] / ROIVolume) * 100.0 << endl;
+            }
         }
         fout.close();
     }
 
     if (plot) {
-        string cmd = "python3 plotDRVH.py -label1 " + fileLabel + " -dir1 " + dir + 
-	  " -roi " + allROINames   ;
+        string cmd = "python3 plotDRVH.py -label1 " + fileLabel + " -dir1 " + dir + " -roi " + allROINames;
         system(cmd.c_str());
     }
 
